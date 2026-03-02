afd39ce2fe7bf16666800d38216d98057d00305e
cirojao <cirojao@himalayasoft.xyz>
2026-03-02 17:33:26 +0800

    chore: firebase init

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,15 @@
 import type { NextConfig } from "next";
 
+// docker
+// const nextConfig: NextConfig = {
+//   output: "standalone",
+// };
+
+// firebase
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: "standalone",
+  output: "export", // 產出純靜態
+  images: { unoptimized: true }, // Firebase Hosting 沒有 Next Image server
+  trailingSlash: true, // 建議：避免路由在 hosting 上 404（可選）
 };
 
 export default nextConfig;
